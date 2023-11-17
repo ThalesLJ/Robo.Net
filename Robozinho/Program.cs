7c70387ec3bf38c9cd22c6ecf90799e72767a2cf
Thales Lima <thales@inovaebiz.com.br>
2023-11-17 14:29:59 -0300

    Adding existing project

---
+++ Robozinho/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Robozinho
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) => services.AddHostedService<Service>())
+            .Build()
+            .Run();
+        }
+    }
+}
---
+++ Robozinho/Service.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Robozinho
+{
+    public class Service: IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine("Run");
+                await Task.Delay(1000);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
